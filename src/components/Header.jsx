70d09c7b166c04fb36cfc16a7d28e5800f27be77
MichaelDWilliams1 <102554995+MichaelDWilliams1@users.noreply.github.com>
2022-11-02 21:31:39 -0700

    nav bar mobile responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,16 +10,16 @@ const {siteData} = useContext(SiteDataContext)
   return (
     <div className='headerBackground bg-neutral-900 text-gray-50 h-5/6 w-729px'>
         <hr className=' w-10/12 ml-auto mr-auto border-1 border-white-500'></hr>
-        <header className='flex justify-center lg:justify-start md:items-center md:w-full m-auto w-5/6 min-h-full h-full'>
+        <header className='flex w-full justify-center lg:justify-start md:items-center md:w-full h-full'>
 <div className='h-82 flex md:items-center md:justify-center md:h-full'>
-    <div className='background flex flex-col justify-between md:justify-around   h-3/4 md:h-2/4 w-5/6 m-auto mt-20 md:m-auto'>
-       <section className='flex '><div className='w-16 text-zinc-500 md:w-5/12  md:flex md:justify-end md:ml-4 md:mr-4'>N E W</div><div className='text-zinc-500' >P R O D U C T</div></section> 
-        <section className='flex flex-col'>
-        <div className='text-6xl  md:text-center'>XX99 MARK II</div>
-        <div className='text-6xl md:text-center'>HEADPHONES</div>
+    <div className='background flex flex-col justify-between md:justify-around h-3/4 md:h-2/4 md:w-5/6 md:m-auto'>
+       <section className='lg:ml-10 flex md:mr-16 md:justify-start'><div className='w-16 text-zinc-500 md:w-2/4 lg:w-auto md:flex md:justify-end lg:justify-start md:ml-4 md:mr-4'>N E W</div><div className='text-zinc-500 w-1/2' >P R O D U C T</div></section> 
+        <section className='lg:ml-10 flex flex-col ml-auto mr-auto w-4/5'>
+        <div className='md:text-6xl text-5xl text-center md:text-center lg:text-left'>XX99 MARK II</div>
+        <div className='text-5xl text-center md:text-6xl md:text-center lg:text-left'>HEADPHONES</div>
         </section>
-        <section className='text-lg flex-wrap w-5/6 font-medium text-zinc-400 md:text-center md:w-3/4 md:ml-auto md:mr-auto'>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</section>
-       <Link to={`/item/${siteData[0]?.product[3].slug}`}><button className=' w-2/6 h-20 bg-orange-600 hover:bg-orange-500 ml-60 mr-60'>SEE PRODUCT</button></Link>
+        <section className='lg:ml-10 ml-auto mr-auto  text-lg flex-wrap w-5/6 md:font-medium text-zinc-400 text-center lg:text-left md:w-3/4'>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</section>
+       <Link className='ml-auto mr-auto border-2 w-60 flex lg:ml-10 items-start ' to={`/item/${siteData[0]?.product[3].slug}`}><button className='w-full h-20 bg-orange-600 hover:bg-orange-500'>SEE PRODUCT</button></Link>
     </div>
 </div>
 <div className='h-82 w-2/4 md:h-0 md:w-0'></div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -70,11 +70,11 @@ if(toggleHamburgerMenu === 'visible'){
 
   return (
     <>
-    <div className='flex h-24  bg-neutral-900 text-white'>
-     <div className='lg:hidden mt-10 ml-20'><i onClick={toggleMenu} className="uil uil-bars md:hover:text-orange-600"></i></div>
-    <Link to='/'><div className='flex h-1/2  w-auto mt-7 ml-40 md:ml-10 lg:ml-32 text-2xl font-bold items-center hover:text-orange-600'>audiophile</div></Link>
+    <div className='flex md:h-24 bg-neutral-900 text-white'>
+     <div className='lg:hidden md:mt-10 md:ml-20 mt-5 ml-5'><i onClick={toggleMenu} className="uil uil-bars md:hover:text-orange-600"></i></div>
+    <Link to='/'><div className='flex h-1/2 border-2 ml-5 mt-5 w-auto md:mt-7  md:ml-10 lg:ml-32 text-2xl font-bold items-center hover:text-orange-600'>audiophile</div></Link>
         <nav className='flex w-auto h-1/2 m-auto mt-7'>
-            <div className='lg:flex lg:w-full sm:hidden lg:visible' >
+            <div className='lg:flex lg:w-full hidden lg:visible' >
                 <ul className='flex items-center space-x-16'>
                     <Link className='hover:text-orange-600' to='/' ><li>HOME</li></Link>
                     <Link className='hover:text-orange-600' to='/headphonesproductpage'><li>HEADPHONES</li></Link>
@@ -86,13 +86,14 @@ if(toggleHamburgerMenu === 'visible'){
 
             <div className=''></div>
         </nav>
-        <motion.nav animate={{width: '200'}}   className='flex h-1/2 m-auto mt-7 absolute top-14 w-60'>
+        <motion.nav animate={{width: '200'}}   className='flex h-1/2 m-auto mt-7 absolute top-6 md:top-14 w-60'>
             <div className={`flex lg:w-3/6 lg:hidden ${toggleHamburgerMenu} w-60`} >
-                <ul className='flex flex-col items-center space-y-1  w-60 mt-3'>
-                    <Link className='hover:text-orange-600 hover:bg-white bg-orange-600 w-60 h-10 rounded-md flex justify-center items-center' to='/' ><li className='focus:text-orange-600'>HOME</li></Link>
-                    <Link className='hover:text-orange-600 hover:bg-white bg-orange-600 w-60 h-10 rounded-md flex justify-center items-center' to='/headphonesproductpage'><li>HEADPHONES</li></Link>
-                    <Link className='hover:text-orange-600 hover:bg-white bg-orange-600 w-60 h-10 rounded-md flex justify-center items-center' to='/speakerproductpage'><li>SPEAKERS</li></Link>
-                    <Link className='hover:text-orange-600 hover:bg-white bg-orange-600 w-60 h-10 rounded-md flex justify-center items-center' to='/earphoneproductpage'><li>EARPHONES</li></Link>
+                <ul className='flex flex-col items-center space-y-1  w-60 md:mt-3 mt-0'>
+                    <Link className='hover:text-orange-600 hover:bg-white bg-orange-600 w-60 h-10 rounded-md flex justify-center items-center border-2' to='/' ><li className='focus:text-orange-600'>HOME</li></Link>
+                    <Link className='hover:text-orange-600 hover:bg-white bg-orange-600 w-60 h-10 rounded-md flex justify-center items-center border-2' to='/headphonesproductpage'><li>HEADPHONES</li></Link>
+                    <Link className='hover:text-orange-600 hover:bg-white bg-orange-600 w-60 h-10 rounded-md flex justify-center items-center border-2' to='/speakerproductpage'><li>SPEAKERS</li></Link>
+                    <Link className='hover:text-orange-600 hover:bg-white bg-orange-600 w-60 h-10 rounded-md flex justify-center items-center border-2' to='/earphoneproductpage'><li>EARPHONES</li></Link>
+                    <div id={signInOrSignOutID} onClick={(e) => openModalPopUp(e)} className='hover:text-orange-600 hover:bg-white bg-orange-600 w-60 h-10 rounded-md flex justify-center items-center border-2'><li>{signInOrProfile}</li></div>
                 </ul>
             </div>
             <div className='flex justify-end items-center w-1/6'></div>
@@ -100,10 +101,10 @@ if(toggleHamburgerMenu === 'visible'){
             <div className=''></div>
         </motion.nav>
         <div id={signInOrSignOutID} onClick={(e) => openModalPopUp(e)}
-            className='bg-orange-600 rounded-lg w-24 h-2/5 mt-7 flex justify-center items-center cursor-pointer hover:bg-orange-500'>
+            className='bg-orange-600 rounded-lg h-1/5 w-20 border-2 mt-5 md:w-24 md:h-2/5 md:mt-7 hidden md:flex  justify-center items-center cursor-pointer hover:bg-orange-500'>
               {signInOrProfile}
           </div> 
-        <i onClick={modalCartPopUpOpen} className="text-4xl ml-10 mt-7 mr-28 uil uil-shopping-cart relative hover:text-orange-600"><CartItemsCounter /></i>
+        <i onClick={modalCartPopUpOpen} className="text-4xl mt-3 ml-3 md:ml-10 md:mt-7 md:mr-28 uil uil-shopping-cart relative hover:text-orange-600"><CartItemsCounter /></i>
        
         {modalSignIn && <SignIn modalPopUp={modalPopUp} />}
         {modalItemsInCart && <CheckoutPreview modalCartPopUpClose={modalCartPopUpClose} />}
