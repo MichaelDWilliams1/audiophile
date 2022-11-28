7774c712c1e7579f26ffdd537fdc0d3657262b92
MichaelDWilliams1 <102554995+MichaelDWilliams1@users.noreply.github.com>
2022-11-28 11:03:20 -0800

    headphones product page resized for tablets

--- src/pages/HeadphonesProduct.jsx
+++ src/pages/HeadphonesProduct.jsx
@@ -16,60 +16,59 @@ const navigate = useNavigate();
         <hr className='w-10/12 m-auto border-1 border-white-500'></hr>
       <div className="flex justify-center items-center h-full w-full">HEADPHONES</div>
     </header>
-    <section className=" h-fit mt-24 w-full">
-        <div className=" flex h-100 w-9/12 m-auto">
-            <div className=" w-1/2 h-full">
+    <section className="h-fit mt-24 w-full">
+        <div className="flex flex-col lg:flex-row w-11/12 lg:w-9/12 m-auto border-2 border-red-600">
+            <div className="lg:w-1/2 h-1/2 lg:h-full">
                 <img className="h-full w-full" src={siteData[0]?.product[3]?.image.desktop} />
             </div>
-            <div className=" flex justify-end items-center w-1/2 h-100">
-                <div className="flex flex-col justify-between h-4/6 w-5/6">
-                    <div className="flex">
+            <div className="flex justify-end items-center w-full lg:w-1/2 h-100 border-2">
+                <div className="flex flex-col justify-between h-4/6 lg:w-5/6 space-y-6">
+                    <div className="flex justify-center lg:justify-start mt-6 lg:mt-0">
                     <div className='w-16 text-zinc-500'>N E W</div>
                     <div className='text-zinc-500' >P R O D U C T</div>
                     </div>
-                    <div className="">
+                    <div className="text-center lg:text-left">
                         <div className="text-5xl">XX99 MARK II</div>
                         <div className="text-5xl">HEADPHONES</div>
                     </div>
                     <div>
-                        <p className="text-md">The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.</p>
+                        <p className="text-md text-center lg:text-left">The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.</p>
                     </div>
-                    <Link to={`/item/${siteData[0]?.product[3].slug}`}><button className="w-2/6 h-20 bg-orange-600 hover:bg-orange-500 text-zinc-200">SEE PRODUCT</button></Link>
+                    <Link className="flex justify-center lg:justify-start" to={`/item/${siteData[0]?.product[3].slug}`}><button className="md:w-2/6 w-5/6 h-20 bg-orange-600 hover:bg-orange-500 text-zinc-200">SEE PRODUCT</button></Link>
                 </div>
            </div>
         </div>
-        <div  className="flex h-100 w-9/12 m-auto mt-24">
-            <div className="flex items-center w-1/2 h-100">
-            <div className="flex flex-col justify-evenly h-4/6 w-5/6">
-                    <div>
+        <div  className="flex flex-col lg:flex-row  w-11/12 lg:w-9/12 m-auto mt-24 border-2 border-red-900">
+            <div className="flex order-1 lg:-order-none items-center w-full lg:w-1/2">
+            <div className="flex flex-col justify-evenly h-4/6 lg:w-5/6 space-y-6">
+                    <div className="text-center mt-6 lg:mt-0">
                         <div className="text-5xl">XX99 MARK I</div>
                         <div className="text-5xl">HEADPHONES</div>
                     </div>
                     <div>
-                        <p className="text-md">As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go.</p>
+                        <p className="text-md text-center lg:text-left">As the gold standard for headphones, the classic XX99 Mark I offers detailed and accurate audio reproduction for audiophiles, mixing engineers, and music aficionados alike in studios and on the go.</p>
                     </div>
-                   <Link to={`/item/${siteData[0]?.product[2].slug}`}><button className="w-2/6 h-20 bg-orange-600 hover:bg-orange-500 text-zinc-200">SEE PRODUCT</button></Link>
+                   <Link className="flex justify-center lg:justify-start" to={`/item/${siteData[0]?.product[2].slug}`}><button className="md:w-2/6 w-5/6 h-20 bg-orange-600 hover:bg-orange-500 text-zinc-200">SEE PRODUCT</button></Link>
                 </div>
             </div>
-            <div className=" w-1/2 h-full">
+            <div className="lg:w-1/2 h-1/2 lg:h-full">
             <img className="h-full w-full" src={siteData[0]?.product[2]?.image.desktop} />
             </div>
-            
         </div>
-        <div  className="flex h-100 w-9/12 m-auto mt-24">
-            <div className="w-1/2 h-full">
+        <div  className="flex flex-col lg:flex-row  w-11/12 lg:w-9/12 m-auto mt-24 border-2 border-red-900">
+            <div className="lg:w-1/2 h-1/2 lg:h-full">
             <img className="h-full w-full" src={siteData[0]?.product[1]?.image.desktop} />
             </div>
-            <div className=" flex justify-end items-center w-1/2 h-100">
-            <div className="flex flex-col justify-evenly h-4/6 w-5/6">
-                    <div>
+            <div className=" flex justify-end items-center lg:w-1/2 h-100">
+            <div className="flex flex-col justify-evenly h-4/6 lg:w-5/6 space-y-6">
+                    <div className="text-center mt-6 lg:mt-0">
                         <div className="text-5xl">XX99 MARK II</div>
                         <div className="text-5xl">HEADPHONES</div>
                     </div>
                     <div>
-                        <p className="text-md">The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.</p>
+                        <p className="text-md text-center lg:text-left">The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.</p>
                     </div>
-                   <Link to={`/item/${siteData[0]?.product[1].slug}`}> <button className="w-2/6 h-20 bg-orange-600 hover:bg-orange-500 text-zinc-200">SEE PRODUCT</button></Link>
+                   <Link className="flex justify-center lg:justify-start" to={`/item/${siteData[0]?.product[1].slug}`}> <button className="md:w-2/6 w-5/6 h-20 bg-orange-600 hover:bg-orange-500 text-zinc-200">SEE PRODUCT</button></Link>
                 </div>
             </div>
         </div>
